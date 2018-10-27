cf4b2cb75dca9ea2b8d5525e6f62dd6f97aff40d
lisknonanika <lisknonanika@gmail.com>
2018-10-27 11:20:38 +0900

    use: node-config

---
+++ config.js
@@ -0,0 +1,16 @@
+const lisk = require('lisk-elements').default;
+const config = require('config');
+
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value: value,
+        enumerable: false,
+        writable: false,
+        configurable: false
+    });
+}
+
+var client = config.mode === 'test'? lisk.APIClient.createTestnetAPIClient():
+                                     lisk.APIClient.createMainnetAPIClient();
+define('liskclient', client);
+define('lisk', config.lisk);
--- lisk/withdraw.js
+++ lisk/withdraw.js
@@ -1,18 +1,18 @@
 const lisk = require('lisk-elements').default;
-const lconf = require('./config');
+const config = require('../config');
 const util = require('../util');
 
 module.exports = function(amount, recipientId){
-    if (util.isNumber(amount) === false || amount < 0.00000001) return;
+    if (util.isNumber(amount) === false || amount < 0.00000001 || config.lisk.passphrase.length === 0) return;
     var params = {};
     params['amount'] = +amount * 100000000;
     params['recipientId'] = recipientId;
-    params['passphrase'] = lconf.passphrase;
-    if (lconf.secondPassphrase.length > 0) params['secondPassphrase'] = lconf.secondPassphrase;
+    params['passphrase'] = config.lisk.passphrase;
+    if (config.lisk.secondPassphrase.length > 0) params['secondPassphrase'] = config.lisk.secondPassphrase;
 
     var trxstr = lisk.transaction.transfer(params);
     console.log(trxstr);
-    lconf.client.transactions.broadcast(trxstr)
+    config.liskclient.transactions.broadcast(trxstr)
         .then(res => {
             console.log(res.data);
         });
